9f7680929db4c482f29e252760d28138f7976eb8
abdul-rayan500apps <rayan.abdul53@gmail.com>
2022-04-17 15:53:15 +0530

    INDEX

--- practice/src/router/index.js
+++ practice/src/router/index.js
@@ -4,7 +4,8 @@ import HelloWorld from '../components/HelloWorld'
 import AbouT from '@/components/About'
 import EvenT from '@/components/Event'
 import RenderinG from'@/components/Rendering'
-
+import ForLoop from "@/components/ForLoop"
+import HomE from '@/components/Home'
 
 vue.use (Router)
 
@@ -15,7 +16,11 @@ const routes = [
 
     {path:'/event',name:"EvenT",component:EvenT},
 
-    {path:'/rendering',name:"RenderinG",component:RenderinG}
+    {path:'/rendering',name:"RenderinG",component:RenderinG},
+
+    {path:'/forloop',name:"ForLoop",component:ForLoop},
+
+    {path:'/home',name:"HomE",component:HomE}
 ]
 
 
