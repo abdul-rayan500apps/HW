22acf7fa2e23f3adb8259eb145936a98710eb919
abdul-rayan500apps <rayan.abdul53@gmail.com>
2022-04-17 07:00:00 +0530

    event

---
+++ practice/src/components/Event.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>How to use click event</h1>
+        <button v-on:click="hello('hello this is Abdul')">Hello call function</button>
+        <button v-on:mouseover="hello('hello this is Abdul')">Hello call function</button>
+    </div>
+</template>
+<script>
+
+
+export default {
+    name:"EvenT",
+
+
+
+    methods:{
+        hello(data)
+        {
+            console.log("Hello console called")
+            alert(data)
+        }
+    }
+}
+</script>
