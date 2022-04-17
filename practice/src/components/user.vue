3c04c82bcec739a98d3b0a850458e685f93e35c3
abdul-rayan500apps <rayan.abdul53@gmail.com>
2022-04-17 15:53:08 +0530

    USERCHILD

---
+++ practice/src/components/user.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>user components</h1>
+    </div>
+</template>
+<script>
+
+
+export default {
+    name:"UserS"
+
+}
+</script>
+<style scoped>
+.users{
+    background-color: green;
+    height: 100;
+    width: 300;
+    padding: 30px;
+    margin:20px
+}
+
+
+</style>
