d849f4473bc82370d3b62520f963602d8b62855e
abdul-rayan500apps <rayan.abdul53@gmail.com>
2022-04-17 07:00:10 +0530

    rendering

---
+++ practice/src/components/Rendering.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1 v-if="show">conditional rendering in vuejs</h1>
+        <h1 v-else>conditional rendeing else block in vuejs</h1>
+        <button v-on:click="display">Toggle</button>
+    </div>
+</template>
+<script>
+
+
+export default {
+    name:"RenderinG",
+
+
+    data()
+    {
+        return{show:true}
+    },
+    methods:
+    {
+        display()
+        {
+            this.show=!this.show
+        }
+        
+    }
+    
+}
+</script>
