1a9e874180ed01c0caa083c80188673dac9f35f2
abdul-rayan500apps <rayan.abdul53@gmail.com>
2022-04-17 15:52:47 +0530

    HOMECOMPONENT

---
+++ practice/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Home component</h1>
+        <UserS />
+        <ProducT />
+    </div>
+</template>
+<script>
+
+import  UserS from '@/components/user';
+import ProducT from '@/components/Product'
+
+
+export default {
+    name:"HomE",
+    components:{
+        UserS,
+        ProducT
+        
+
+    }
+}
+</script>
