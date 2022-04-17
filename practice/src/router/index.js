8b96884297c75c5451915984ddafec0e66b04af3
abdul-rayan500apps <rayan.abdul53@gmail.com>
2022-04-17 07:00:18 +0530

    index

--- practice/src/router/index.js
+++ practice/src/router/index.js
@@ -2,17 +2,20 @@ import vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '../components/HelloWorld'
 import AbouT from '@/components/About'
+import EvenT from '@/components/Event'
+import RenderinG from'@/components/Rendering'
 
 
 vue.use (Router)
 
 const routes = [
-    {
-        path:'/',
-        name:"Home",
-        component:HelloWorld
-    },
-    {path:'/about',name:"AbouT",component:AbouT}
+    { path:'/',name:"Home",component:HelloWorld},
+
+    {path:'/about',name:"AbouT",component:AbouT},
+
+    {path:'/event',name:"EvenT",component:EvenT},
+
+    {path:'/rendering',name:"RenderinG",component:RenderinG}
 ]
 
 
