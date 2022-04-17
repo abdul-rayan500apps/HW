12d0c53f28a4c0ddc77cab930483c6acfd56b760
abdul-rayan500apps <rayan.abdul53@gmail.com>
2022-04-17 15:52:58 +0530

    PRODUCTCHILD

---
+++ practice/src/components/Product.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>product component</h1>
+    </div>
+</template>
+<script>
+
+
+export default {
+    name:"ProducT"
+
+}
+</script>
+<style scoped>
+.users{
+    background-color: green;
+    height: 100;
+    width: 300;
+    padding: 30px;
+    margin:20px
+}
+
+
+</style>
