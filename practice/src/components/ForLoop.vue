a99bffe60ddde4a4a31c3b5da7db02ea74e7caf1
abdul-rayan500apps <rayan.abdul53@gmail.com>
2022-04-17 15:52:37 +0530

    FORLOOP

---
+++ practice/src/components/ForLoop.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>
+            For Loop in vuejs
+        </h1>
+        <table border="1px" align="center">
+            <tr>
+                <td>id</td>
+                <td>name</td>
+                <td>Email</td>
+            </tr>
+            <tr v-for="user in users" :key="user">
+                <td>{{user.id}}</td>
+                <td>{{user.name}}</td>
+                <td>{{user.Email}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script>
+
+
+export default {
+    name:"ForLoop",
+
+
+    data()
+    {
+        return{
+            users:[
+                {id:1,name:'abdul',Email:'abdul.500'},
+                {id:2,name:'mohd',Email:'mohd.500'},
+                {id:3,name:'ahmed',Email:'ahmed.500'}
+            ]
+        }
+    }
+}
+</script>
