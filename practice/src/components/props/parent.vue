e9c6e1ecd9fed06319f09de0766d57b99e0788cc
abdul-rayan500apps <rayan.abdul53@gmail.com>
2022-04-24 15:35:54 +0530

    props parent to child

---
+++ practice/src/components/props/parent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>here we sending parent yo child</h1>
+        <h1>props in vuejs ///video 12th</h1>
+        <child v-bind:username="username"/>
+       <child v-bind:users="users"/>
+
+    </div>
+</template>
+<script>
+
+
+import Child from './Child.vue'
+export default {
+  components: { Child },
+    name:"ParenT",
+    data(){
+        return{
+            username:'rehan',
+            users:[{name:"rehan" ,email:"rehan@"},{name:"rayan" ,email:"rayan@"},{name:"rizwan" ,email:"rizwan@"}]
+            
+            }
+    }
+   
+}
+</script>
