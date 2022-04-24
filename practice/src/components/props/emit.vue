172b48fb3066b027e2b212e89c0d956e7ada4654
abdul-rayan500apps <rayan.abdul53@gmail.com>
2022-04-24 15:35:28 +0530

    emit-component

---
+++ practice/src/components/props/emit.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <button v-on:click="updatetittle()">update parent tittle</button>
+    </div>
+</template>
+<script>
+
+
+export default {
+    name:"EmiT",
+
+methods:{
+updatetittle(){
+   // console.warn("hello from tittle")
+   this.$emit('changetittle',"parent updated")  
+}
+}
+}
+</script>
