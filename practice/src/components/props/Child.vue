fa74ca6062c4307faeacd4f7bb3f76d3eb515d6f
abdul-rayan500apps <rayan.abdul53@gmail.com>
2022-04-24 15:35:41 +0530

    child component

---
+++ practice/src/components/props/Child.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>child component</h1>
+       <h3>{{name}}</h3>
+        <h3 v-for="user in users" :key="user.name"> {{user.name}}---{{user.email}}</h3>
+        
+    </div>
+</template>
+<script>
+
+
+export default {
+    name:"ChilD",
+    props:['users'],
+    
+    
+}
+</script>
