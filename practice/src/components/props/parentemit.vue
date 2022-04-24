3cb6341ccdfb8939b21dd2709484fe73d2e941cf
abdul-rayan500apps <rayan.abdul53@gmail.com>
2022-04-24 15:35:09 +0530

    emit-childtoparent

---
+++ practice/src/components/props/parentemit.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+   <!-- <h1>send props child to parent</h1>-->
+  <h2> --video 13 --></h2>
+    <h2>{{tittle}}</h2> 
+    <EmiT v-on:changetittle="updatetittle($event)"/>
+  </div>
+</template>
+
+<script>
+import EmiT from '@/components/props/emit'
+export default {
+  name: 'App',
+  components: {EmiT},
+  data(){
+      return{tittle:"props tutorial"}
+  },
+  methods:{
+updatetittle(tittle){
+    this.tittle=tittle
+}
+  }
+    
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
