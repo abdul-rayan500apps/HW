a0a4a322c656423bf44efc9ab7822dec6cd0a819
abdul-rayan500apps <rayan.abdul53@gmail.com>
2022-04-30 16:19:08 +0530

    slotchild

---
+++ practice/src/components/props/slotchild.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+<div >
+    <slot name="title"/>
+</div>
+<hr>
+<div >
+    <slot name="content"/>
+</div>
+    </div>
+</template>
+<script>
+
+
+export default {
+    name:"SlotchilD",
+    
+}
+</script>
+<style scoped>
+div{
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    padding: 30px;
+    margin: 30px auto;
+    text-align:center;
+}
+h2{
+    color:blue
+}
+.title{
+    font-style:italic;
+}
+</style>
